0ea88cd3b9bb2c3ad3b348c0fc4a9da4fca93da3
Jye Lewis <jye@jyelewis.com>
2022-12-06 17:42:34 +1100

    Day 6

---
+++ 06/06.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync(__dirname + "/input.txt").toString();
+
+function findEndOfFirstMarker(markerLength: number) {
+  // scan through the string, character by character
+  for (let i = 0; i < input.length - markerLength; i++) {
+    // look forward for a marker
+    const potentialMarker = input.slice(i, i + markerLength);
+
+    // check if all characters in our potential marker are unique
+    if (new Set(potentialMarker).size === markerLength) {
+      // x unique characters!
+      // return the position of the END of the marker (cursor is at the start, looking forward)
+      return i + markerLength;
+    }
+  }
+
+  throw new Error("Marker never found");
+}
+
+console.log("4", findEndOfFirstMarker(4));
+console.log("14", findEndOfFirstMarker(14));
